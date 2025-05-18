a06ebf5cf20fadba0829ff6e2229c9d1ffd55862
jxbox <68604733+jxbox@users.noreply.github.com>
2025-05-18 12:57:50 +0000

    env config

--- version.tf
+++ version.tf
@@ -8,6 +8,6 @@ terraform {
 }
 provider "huaweicloud" {
   region     = "me-east-1"
-  access_key = "MUDXLHNXB11KBWCV1RGU"
-  secret_key = "RnP4uxQaF8AM9GTYwhE69soWcPF3b3sFHCWfDl6Q"
+  #access_key = "MUDXLHNXB11KBWCV1RGU"
+  #secret_key = "RnP4uxQaF8AM9GTYwhE69soWcPF3b3sFHCWfDl6Q"
 }
